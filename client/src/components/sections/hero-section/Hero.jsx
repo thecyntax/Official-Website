1f04fbd87f71207caf4f191fbf14b09afb7a086b
amalthorakkat <amalthorakkat@gmail.com>
2026-03-17 22:14:47 +0530

    Started Hero Section - Added background image - Adjusted the height and margin as needed

--- client/src/components/sections/hero-section/Hero.jsx
+++ client/src/components/sections/hero-section/Hero.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="m-2 mt-0">
+      <div className="hero-section-bg min-h-[60vh] lg:h-[92vh] rounded-2xl  w-full flex items-center justify-center  ">
+        <h1 className="text-white ">hero</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
