66434c00122d5ad7c6654c066a81b5f5c4d6e6d7
SufailSLX <sufailxd@gmail.com>
2026-03-17 09:41:53 +0530

    Changes -Header Done -Project Section Done -more changes needed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import UserLayout from './components/layout/UserLayout'
 import Home from './components/Home'
+import Project from './components/sections/Project/Project'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<UserLayout />}>
-          <Route index element={<Home />} />
-        </Route>
+        <Route path="/" element={<UserLayout />} />
       </Routes>
     </div>
   )
