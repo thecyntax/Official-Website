29aa77d1a2cd15680c222ddf6dd39d7db3a89483
amalthorakkat <amalthorakkat@gmail.com>
2026-03-27 22:28:26 +0530

    update in hero section

--- client/src/components/sections/hero-section/HeroDesktop.jsx
+++ client/src/components/sections/hero-section/HeroDesktop.jsx
@@ -1,63 +1,3 @@
-// import React from "react";
-// import { Plus } from "lucide-react";
-// import Card from "./Card";
-
-// const Hero = () => {
-//   return (
-//     <div className="m-2 mt-0 hidden lg:block ">
-//       <div className="hero-section-bg min-h-[60vh] lg:h-[92vh] rounded-2xl p-15  w-full flex flex-col justify-center ">
-//         <div className="text-white flex items-end justify-between">
-//           <div>
-//             <h1 className="leading-[0.5]">
-//               <span className="text-[60px] font-medium ml-3 "> the</span> <br />
-//               <span className="text-[300px] font-semibold ">syntax</span>
-//             </h1>
-//             <div className="flex items-center justify-between my-[170px] ">
-//               <Plus className="text-[#e4e4e4]" />
-//               <Plus className="text-[#e4e4e4]" />
-//               <Plus className="text-[#e4e4e4]" />
-//             </div>
-//           </div>
-
-//           <div>
-//             <div className="font-bold text-[17px] flex flex-col gap-2 ">
-//               <p>Intuitive UI/UX Design </p>
-//               <p>Cross-Platform Mobile Apps </p>
-//               <p>Full-Stack Digital Platforms </p>
-//               <p>Growth-Driven Digital Marketing </p>
-//             </div>
-//             <div className="my-[170px]">
-//               <Plus className="text-[#e4e4e4]" />
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="flex justify-between items-end ">
-//           <div className="max-w-[600px]">
-//             <p className="text-[#dddddd] text-[22px] font-medium indent-10 leading-[1.1] max-w-[600px]">
-//               No generic solutions. No empty promises.
-//               <span className="text-[#767676]">
-//                 Just powerful digital experiences that help your business grow
-//                 and your brand stand out.
-//               </span>
-//             </p>
-//           </div>
-
-//           <div className="text-white flex justify-between gap-10 items-end">
-//             <p className=" text-[14px] " >© {new Date().getFullYear()} the syntax.</p>
-//             <div>
-//               <Card/>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
-
-
 import React from "react";
 import { Plus } from "lucide-react";
 import Card from "./Card";
@@ -65,13 +5,16 @@ import Card from "./Card";
 const Hero = () => {
   return (
     <div className="m-2 mt-0 hidden lg:block">
-      <div className="hero-section-bg min-h-[60vh] xl:h-[92vh] rounded-2xl xl:p-15 w-full flex flex-col justify-center  lg:h-[92vh] lg:p-10 ">
+      <div className="hero-section-bg min-h-[60vh] xl:h-[92.2vh] rounded-2xl xl:p-15 w-full flex flex-col justify-center  lg:h-[92vh] lg:p-10 ">
         <div className="text-white flex items-end justify-between">
-          
           <div>
-            <h1 className="leading-[0.5]">
-              <span className="xl:text-[60px] font-medium ml-3 lg:text-[40px] ">the</span> <br />
-              <span className="xl:text-[300px] font-semibold lg:text-[200px]">syntax</span>
+            <h1 className="flex flex-col tracking-tighter text-white leading-[0.1]">
+              <span className="xl:text-[60px] font-medium ml-3 lg:text-[40px] lg:-mb-8 xl:-mb-12">
+                the
+              </span>
+              <span className="xl:text-[300px] font-bold lg:text-[200px] leading-[0.85]">
+                syntax.
+              </span>
             </h1>
 
             <div className="flex items-center justify-between xl:my-42.5 lg:my-37.5">
@@ -115,7 +58,6 @@ const Hero = () => {
             </div>
           </div>
         </div>
-
       </div>
     </div>
   );
