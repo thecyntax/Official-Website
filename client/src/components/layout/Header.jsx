ff54f952357892a7c8e15c2996dc84469e0d86b0
amalthorakkat <amalthorakkat@gmail.com>
2026-04-03 20:12:08 +0530

    header changes

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -15,7 +15,6 @@ const Header = () => {
     <>
       {/* HEADER */}
       <header className="fixed top-0 left-0 w-full h-[64px] px-6 flex items-center justify-between bg-[#F5F5F5] md:px-10 md:justify-start lg:justify-between md:gap-10 z-50">
-      
         {/* Logo */}
         <img src={Logo} alt="The Cyntax Logo" className="h-5 md:h-6.25" />
         {/* Desktop Nav */}
